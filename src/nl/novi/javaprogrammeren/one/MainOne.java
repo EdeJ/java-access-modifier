37f38577172c59af5b5301a881bee00bc69c00ca
Emiel de Jong <emieldejong@xs4all.nl>
2020-10-01 17:09:03 +0200

    work in progress

--- src/nl/novi/javaprogrammeren/one/MainOne.java
+++ src/nl/novi/javaprogrammeren/one/MainOne.java
@@ -4,7 +4,11 @@ public class MainOne {
     public static void main(String[] args) {
 
         Person sjaak = new Person("Sjaak");
+        System.out.println(sjaak.getName());
 
+        // * 1) maak de methode public
+        // * 2) package private
+        // * 3) protected
 
     }
 }
--- src/nl/novi/javaprogrammeren/one/Person.java
+++ src/nl/novi/javaprogrammeren/one/Person.java
@@ -7,8 +7,14 @@ public class Person {
         this.name = name;
     }
 
-    private String getName() {
+//    public String getName() {
+//        return name;
+//    }
+     String getName() {
         return name;
     }
+//    protected String getName() {
+//        return name;
+//    }
 
 }
