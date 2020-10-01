a3aeffa34f50581286df3c7a148cdb6d57848332
Emiel de Jong <emieldejong@xs4all.nl>
2020-10-01 21:45:40 +0200

    work in progress

--- src/nl/novi/javaprogrammeren/four/MainFour.java
+++ src/nl/novi/javaprogrammeren/four/MainFour.java
@@ -1,7 +1,11 @@
 package nl.novi.javaprogrammeren.four;
 
+
+import nl.novi.javaprogrammeren.four.hidden.PrisonCell;
+
 public class MainFour {
     public static void main(String[] args) {
-
+        PrisonCell prisonCell = new PrisonCell();
+        System.out.println(prisonCell.getInhabitantName());
     }
 }
--- src/nl/novi/javaprogrammeren/four/hidden/PrisonCell.java
+++ src/nl/novi/javaprogrammeren/four/hidden/PrisonCell.java
@@ -1,4 +1,14 @@
 package nl.novi.javaprogrammeren.four.hidden;
 
 public class PrisonCell {
+
+    Inhabitant inhabitant;
+
+    public PrisonCell() {
+        this.inhabitant = new Inhabitant("Emiel");
+    }
+
+    public String getInhabitantName() {
+        return inhabitant.getName();
+    }
 }
